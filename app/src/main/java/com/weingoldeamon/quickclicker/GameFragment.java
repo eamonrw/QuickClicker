ac12adccac77b4532da985bd6fb1aa08afb0274b
eamonrw <eamonweingold@gmail.com>
2020-12-07 02:33:28 -0500

    Final Commit. Full mobile-web cross implementation complete.

--- app/src/main/java/com/weingoldeamon/quickclicker/GameFragment.java
+++ app/src/main/java/com/weingoldeamon/quickclicker/GameFragment.java
@@ -6,11 +6,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
-
-import java.util.ArrayList;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import org.json.JSONObject;
+import java.util.HashMap;
 
 public class GameFragment extends Fragment {
 
@@ -18,7 +22,8 @@ public class GameFragment extends Fragment {
     long cur_time;
     TextView infoText;
     CountDownTimer waitTimer;
-    ArrayList<Double> scoreData = new ArrayList<>();
+    RequestQueue requestQueue;
+    String url = "https://proficiency-lab.sites.tjhsst.edu/post-leaderboard";
 
     public GameFragment() { }
     @Override
@@ -26,6 +31,7 @@ public class GameFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_game, container, false);
 
         infoText = view.findViewById(R.id.info_text);
+        requestQueue = RequestQueueSingleton.getInstance(getActivity().getApplicationContext()).getRequestQueue();
 
         view.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,13 +39,13 @@ public class GameFragment extends Fragment {
                 if(status == 0) {
                     screen.setBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.colorWait, null));
                     infoText.setText("Tap when the screen turns green.");
-                    cur_time = System.currentTimeMillis();
+                    cur_time = System.nanoTime();
                     waitTimer = new CountDownTimer((long)(Math.random() * 6000 + 3000), 1000) {
                         public void onFinish() {
                             screen.setBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.colorGo, null));
                             infoText.setText("Tap now!");
                             status = 2;
-                            cur_time = System.currentTimeMillis();
+                            cur_time = System.nanoTime();
                         }
 
                         public void onTick(long millisUntilFinished) {
@@ -56,11 +62,26 @@ public class GameFragment extends Fragment {
                 }
 
                 else {
-                    double reactionTime = (System.currentTimeMillis() - cur_time)/1000.0;
-                    scoreData.add(reactionTime);
+                    double reactionTime = (System.nanoTime() - cur_time)/1000000000.0;
                     screen.setBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.colorStart, null));
-                    infoText.setText("You tapped in " + String.format("%.3f", reactionTime) + " seconds. Tap again to replay.");
+                    infoText.setText("You tapped in " + String.format("%.5f", reactionTime) + " seconds. Tap again to replay.");
                     status = 0;
+
+                    HashMap<String, String> params = new HashMap<String, String>();
+                    params.put("data", String.format("%.5f", reactionTime));
+                    JsonObjectRequest jreq = new JsonObjectRequest(Request.Method.POST, url, new JSONObject(params),
+                            new Response.Listener<JSONObject>() {
+                                @Override
+                                public void onResponse(JSONObject response) {
+                                    System.out.println("Success");
+                                }
+                            }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            error.printStackTrace();
+                        }
+                    });
+                    requestQueue.add(jreq);
                 }
             }
         });
--- app/src/main/java/com/weingoldeamon/quickclicker/LeaderboardFragment.java
+++ app/src/main/java/com/weingoldeamon/quickclicker/LeaderboardFragment.java
@@ -7,10 +7,17 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-
 import androidx.fragment.app.Fragment;
-
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class LeaderboardFragment extends Fragment {
 
@@ -19,27 +26,44 @@ public class LeaderboardFragment extends Fragment {
     ArrayAdapter scoreAdapter;
     ListView scoreList;
     Button clearButton;
+    RequestQueue requestQueue;
+    String url = "https://proficiency-lab.sites.tjhsst.edu/get-leaderboard";
+    String clear_url = "https://proficiency-lab.sites.tjhsst.edu/clear-leaderboard";
 
     public LeaderboardFragment() { }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_leaderboard, container, false);
 
-        /*Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            scoreData = (ArrayList<Double>)(extras.get("scoreData"));
-        }*/
-
         for(int i = 0; i < 10; i++)
             scores[i] = "-- seconds";
 
-        /*Collections.sort(scoreData);
-        int num = 10;
-        if(scoreData.size() < num)
-            num = scoreData.size();
-        for(int i = 0; i < num; i++) {
-            scores[i] = String.format("%.3f", scoreData.get(i)) + " seconds";
-        }*/
+        requestQueue = RequestQueueSingleton.getInstance(getActivity().getApplicationContext()).getRequestQueue();
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+            new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    ArrayList<Double> scr = jsonToList(response);
+                    Collections.sort(scr);
+                    int num = 10;
+                    if(scr.size() < num)
+                        num = scr.size();
+                    for(int i = 0; i < 10; i++)
+                        scores[i] = "-- seconds";
+                    for(int i = 0; i < num; i++)
+                        scores[i] = String.format("%.5f", scr.get(i)) + " seconds";
+
+                    scoreAdapter.notifyDataSetChanged();
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    scores[0] = "Error";
+                    scoreAdapter.notifyDataSetChanged();
+                    error.printStackTrace();
+            }
+        });
+        requestQueue.add(stringRequest);
 
         scoreAdapter = new ArrayAdapter<>(getContext(), R.layout.list_item, scores);
         scoreList = view.findViewById(R.id.score_list);
@@ -49,6 +73,19 @@ public class LeaderboardFragment extends Fragment {
         clearButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, clear_url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                error.printStackTrace();
+                        }
+                });
+                requestQueue.add(stringRequest);
                 for(int i = 0; i < 10; i++)
                     scores[i] = "-- seconds";
                 scoreAdapter.notifyDataSetChanged();
@@ -56,5 +93,21 @@ public class LeaderboardFragment extends Fragment {
         });
 
         return view;
+    }
+
+    public ArrayList<Double> jsonToList(String j) {
+        ArrayList<Double> list = new ArrayList<>();
+        try {
+            JSONArray jArray = new JSONArray(j);
+            for (int i = 0; i < jArray.length(); i++) {
+                JSONObject jObject = jArray.getJSONObject(i);
+                list.add(Double.parseDouble(jObject.getString("time")));
+            }
+        }
+        catch(JSONException e) {
+            System.out.println("JSONException");
+            e.printStackTrace();
+        }
+        return list;
     }
 }
